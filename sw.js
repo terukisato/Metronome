5cbd0e07bd8936800e494a21fe3160194f138b2e
terukisato <117873838+terukisato@users.noreply.github.com>
2026-05-12 13:01:24 +1200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'metronome-v2';
+const CACHE = 'metronome-v4';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
